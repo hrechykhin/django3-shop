d7d6c24518569045f3bd74480d9be83bc4282277
hrechykhin <vladgrechikhin@gmail.com>
2021-09-21 23:43:38 +0300

    Mixins

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView, View
 
 from .models import Notebook, Smartphone, Category
-
-def test_view(request):
-    categories = Category.objects.get_categories_for_left_sidebar()
-    return render(request, 'base.html', {'categories':categories})
+from .mixins import CategoryDetailMixin
 
 
-class ProductDetailView(DetailView):
+class BaseView(View):
+    def get(selfself, request, *args, **kwargs):
+        categories = Category.objects.get_categories_for_left_sidebar()
+        return render(request, 'base.html', {'categories': categories})
+
+
+class ProductDetailView(CategoryDetailMixin, DetailView):
 
     CT_MODEL_MODEL_CLASS = {
         'notebook': Notebook,
@@ -27,7 +30,7 @@ class ProductDetailView(DetailView):
     slug_url_kwarg = 'slug'
 
 
-class CategoryDetailView(DetailView):
+class CategoryDetailView(CategoryDetailMixin, DetailView):
 
     model = Category
     queryset = Category.objects.all()
